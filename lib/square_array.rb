60f081146c968bebe0de88d3cea4bfc6982d92a8
Ashab Ahmed <ashabahmed7@gmail.com>
2020-08-10 05:22:52 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,19 +1,10 @@
-#def square_array(array)
- #counter = 0 
- #new_array = []
- #while counter < array.length do
-#  new_array << array[counter] ** 2
-#  counter += 1 
-# end
-# return new_array
-#end
-
 def square_array(array)
-  new_array = []
-  count = 0 
-  while count < array.length do
-   new_array << array[count] ** 2
-    count += 1 
-  end 
+ counter = 0 
+ new_array = []
+  while counter < array.length do
+  new_array << array[counter] ** 2
+  counter += 1 
+  end
   return new_array
 end
+
